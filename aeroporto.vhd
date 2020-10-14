03366dd67db75af3ea3954c323e7d310e69e6057
Mateus <mateus.magalhaes@upflow.me>
2020-10-14 09:27:24 -0300

    atualizacao dos arquivos PARTE DOIS

--- aeroporto.vhd
+++ aeroporto.vhd
@@ -17,12 +17,53 @@ port(
 end aeroporto;
 
 architecture arch of aeroporto is
-	type estado is (AeroportoFuncionando, Pousando, Decolando, Imprevisto);		--Os 4 estados da nossa FSM
-	signal PE,EA,BACKUP :estado;		--Proximo Estado, Estado Atual, Backup(Esse serve para armazenar algum cancelamento de pouso ou decolagem por tempestade)
+	type estado is (AeroportoFuncionando, Pousando, Decolando, Espera);		--Os 4 estados da nossa FSM
+	signal PE,EA,BACKUP :estado;		--Proximo Estado, Estado Atual, Backup(Esse serve para armazenar algum
+--	cancelamento de pouso ou decolagem por tempestade)
 	
 begin
-	process()
-		variable PistaLivre : boolean;
-	
-	
+	process(clock, EA) -- Toda vez que o clock ou o estado mudar o process deve ser checado
+		variable pistaLivre, pousar, decolar, imprevisto, peso, clima, tempo : std_logic;
+		--variavel tempo é especial e vai ter que ter uma funcao p/ habilitá-la com 1 ou 0
+	begin
+	if (pousar = '0' and decolar = '0') then -- enquanto não há tráfego o estado pernece em AF
+		EA <= AeroportoFuncionando;
+	elsif (clock'event and clock = '1') then
+		case EA is
+		
+			when AeroportoFuncionando => 
+				if (decolar = '1' and imprevisto = '0' and pistaLivre = '1') then -- Somente quando tiver aviao p/ decolar,
+			--	não houver imprevistos e a pista estiver livre ele poderá decolar
+					EA <= Decolando;
+				elsif (pousar = '1' and imprevisto = '0' and peso = '0' and pistaLivre = '1') then
+					EA <= Pousando;
+				elsif (imprevisto = '1' or peso = '1' or pistaLivre = '0') then
+					--EA <= Espera; --?? Quando isso acontece o aviao deve ser mandado pra outro estado de espera, não?
+					EA <= AeroportoFuncionando;
+				elsif (clima = '1') then
+					EA <= Espera;
+				end if;
+				
+			when Decolando =>
+				if (clima = '1') then
+					EA <= Espera;
+				elsif (tempo = '1' and clima = '0') then --mexer dps na variavel tempo
+					EA <= AeroportoFuncionando;
+				end if;
+				
+			when Pousando =>
+				if (clima = '1') then
+					EA <= Espera;
+				elsif (tempo= '1') then
+					EA <= AeroportoFuncionando;
+				end if;
+			
+			when Espera =>
+				if (clima = '0') then
+					EA <= Pousando;
+				end if;
+			
+		end case;
+	end if;
+	end process;
 end arch;
